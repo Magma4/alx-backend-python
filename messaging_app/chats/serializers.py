bb908e4ef89b4fff446bc40519f140946e68e568
Magma4 <youngmagma@protonmail.com>
2024-12-24 23:39:21 +0000

    Changed Charfield to CharField

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import User, Message, Conversation
 
 class UserSerializer(serializers.ModelSerializer):
-    full_name = serializers.Charfield(source='get_full_name')
+    full_name = serializers.CharField(source='get_full_name')
     class Meta:
         model = User
         fields = ['user_id', 'username', 'first_name', 'last_name', 'phone_number', 'role', 'created_at', 'full_name' ]
