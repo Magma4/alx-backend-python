9cc90dc94ae73e2d53c757d2a0eae2a8e463ca9e
Magma4 <youngmagma@protonmail.com>
2024-12-26 16:54:35 +0000

    Build api endpoinrs with viewset

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -16,15 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from chats.views import ConversationViewSet, MessageViewSet
 
-# Create a router and register viewsets
-router = DefaultRouter()
-router.register(r'conversations', ConversationViewSet, basename='conversation')
-router.register(r'messages', MessageViewSet, basename='message')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include(router.urls)), 
+    path('admin/', admin.site.urls), 
+    path('api/', include('chats.urls')),
 ]
