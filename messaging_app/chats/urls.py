a448b5cf453b773b9e8868f64459ddcd669e854e
Magma4 <youngmagma@protonmail.com>
2024-12-26 16:58:25 +0000

    Build api endpoinrs with viewset

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import ConversationViewSet, MessageViewSet 
-
+from rest_framework_nested.routers import DefaultRouter, NestedDefaultRouter
+from .views import ConversationViewSet, MessageViewSet
 
 router = DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
-router.register(r'messages', MessageViewSet, basename='message')
+
+nested_router = NestedDefaultRouter(router, r'conversations', lookup='conversation')
+nested_router.register(r'messages', MessageViewSet, basename='conversation-messages')
 
 urlpatterns = [
-    path('', include(router.urls)), 
+    path('', include(router.urls)),
+    path('', include(nested_router.urls)),
 ]
