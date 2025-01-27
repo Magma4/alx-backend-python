9a6966bd10759f94d5032db634f7b5908655c445
magma <youngmagma@protonmail.com>
2025-01-26 20:47:46 -0500

    django midleware

---
+++ Django-Middleware-0x03/messaging_app/chats/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework_nested.routers import DefaultRouter, NestedDefaultRouter
+from .views import ConversationViewSet, MessageViewSet
+
+router = DefaultRouter()
+router.register(r'conversations', ConversationViewSet, basename='conversation')
+
+nested_router = NestedDefaultRouter(router, r'conversations', lookup='conversation')
+nested_router.register(r'messages', MessageViewSet, basename='conversation-messages')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('', include(nested_router.urls)),
+]
